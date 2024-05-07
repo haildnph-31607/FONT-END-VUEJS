67c62bbbf96cbdaa80a33db9103e2170ca5b0ead
unknown <haildnph31607@fpt.edu.vn>
2024-05-07 13:45:41 +0700

    push 7-5-2024

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ window.axios = axios;
 import "../node_modules/bootstrap/dist/css/bootstrap-grid.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap-utilities.min.css";
 import { Menu,
+        Table,
          List,
          Drawer,
          Button,
+         Card,
          message } from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
 import router from "./router/index.js";
@@ -18,6 +20,8 @@ import App from "./App.vue";
 createApp(App)
 .use(createPinia())
   .use(router)
+  .use(Card)
+  .use(Table)
   .use(Button)
   .use(Drawer)
   .use(List)
--- src/pages/admin/users/index.vue
+++ src/pages/admin/users/index.vue
@@ -1,18 +1,84 @@
 <template>
-    DDaya la trang quan li user
+    <a-card title="Tài Khoản Người Dùng" style="width: 100%">
+        <div class="row">
+            <div class="col-12">
+                <a-table :dataSource="user" :columns="columns" :scroll="{ x: 576 }">  <!-- dataSourece là chứa dataa , colum để setup table -->
+                    <template #bodyCell="{ column, index ,record}">  <!-- sửa table theo ý -->
+                        <template v-if="column.key === 'index'">  <!-- data index để tự sắp xếp -->
+                           <span>{{ index+1 }}</span>
+                        </template>
+                        <template v-if="column.key === 'users_status'">
+                            <span v-if="record.status_id === 1" class="text-primary">{{ record.users_status  }}</span>  <!-- Hiển thị màu status -->
+                            <span v-if="record.status_id === 2" class="text-danger">{{ record.users_status  }}</span>
+
+                         </template>
+                    </template>
+
+                </a-table>
+            
+
+            </div>  
+        </div>
+    </a-card>
 </template>
 <script>
-import {useMenu} from "../../../stores/use-menu.js";
-export default {
+import {useMenu} from "../../../stores/use-menu.js";// kho chưuas store
+import { defineComponent , ref } from "vue";
+export default defineComponent({
+    
     setup(){
-        const store = useMenu();
+        const store = useMenu(); // toàn bộ store nằm trong store/use-menu dùng để đồng bộ view giữa phone và decktop
         store.onSelectedKeys(['admin.users']);
-
+        const user = ref([]); // data được gắn vào thẻ này để return lại
+        const columns =[
+            {
+                title:"#",// tên cột
+                dataIndex:'index',// data
+                key:"index" // key sử dụng trên AntDesign
+            },
+            {
+                title:"Avatar",
+                dataIndex:'avatar',
+                key:"avatar"
+            },
+            {
+                title:"Tài Khoản",
+                dataIndex:'username',
+                key:"username"
+            },
+            {
+                title:"Tên",
+                dataIndex:'name',
+                key:"name"
+            },
+           
+            {
+                title:"Phòng Ban",
+                dataIndex:'departments',
+                key:"departments",
+                responsive: ['sm'],// reponsive cho cột phòng ban nếu là phone thì kh hiển thị
+            },
+            {
+                title:"Vai Trò",
+                dataIndex:'',
+                key:""
+            },
+            {
+                title:"Tình Trạng",
+                dataIndex:'users_status',
+                key:"users_status"
+            },
+            {
+                title:"Công Cụ",
+                fixed: "right",
+                key:""
+            },
+        ]
         const getUser = ()=>{
             axios.get('http://127.0.0.1:8000/api/users')
                .then(function (response) {
-    // handle success
-                 console.log(response);
+                console.log(response.data.data);
+                 user.value = response.data.data;
               })
            .catch(function (error) {
     // handle error
@@ -21,6 +87,10 @@ export default {
         
         };
         getUser();
+        return {
+            user,
+            columns
+        }
      }
-}
+});
 </script>
